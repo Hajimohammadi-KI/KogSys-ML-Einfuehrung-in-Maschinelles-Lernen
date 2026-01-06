b3c4f7b4df28ba6b3c625b05eb11cb0c3011a9d2
Fatemeh Hajimohammadi <education.hajimohammadi@gmail.com>
2026-01-06 22:16:43 +0100

    Add uebung material

---
+++ uebung-Matrialian/4 -6.11.2025 -  Inductive Logic Programming (Pt. 1)/sandwich.pl
@@ -0,0 +1,19 @@
+pos(sandwich(b)).
+pos(sandwich(e)).
+pos(sandwich(f)).
+neg(sandwich(a)).
+neg(sandwich(c)).
+neg(sandwich(d)).
+neg(sandwich(g)).
+
+on_table(a).
+on_table(d).
+
+free(c).
+free(g).
+
+on_block(b, a).
+on_block(c, b).
+on_block(e, d).
+on_block(f, e).
+on_block(g, f).
